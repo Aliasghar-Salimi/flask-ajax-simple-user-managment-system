afbed035e645ec743808ce8826a468717e52c11e
Aliasghar-salimi <aliasghar.salimi.05@gmail.com>
2024-11-17 01:47:12 +0330

    fix cors block bug

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import sqlite3
 import connection
 import json
 # from flask_wtf.csrf import CSRFProtect
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # app.secret_key = "flask-1234"
 # CSRFProtect(app)
